03a7056f66fda48b5e682d16f08836b74e1be3c4
fikachan <fikaaindriani906@gmail.com>
2025-12-03 01:19:51 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -12,14 +12,12 @@ model = joblib.load("klasifikasi_level_coding.joblib")
 st.title("🔥Klasifikasi Level Coding🔥")
 st.markdown("Klasifikasi level coding berdasarkan fitur hours coding daily, preferred language, typing speed, import usage, dan OOP usage")
 
-# Input
 hours_coding_daily = st.slider("hours_coding_daily", 2.5, 5.5, 3.0)
-preferred_language = st.selectbox("preferred_language", ["Python", "C++", "Java"])
+preferred_language = st.pills("preferred_language", ["Python", "C++", "Java"])
 typing_speed = st.slider("typing_speed", 25, 65, 35)
 import_usage = st.pills("import_usage", ["Yes", "No"])
 oop_usage = st.radio("oop_usage", ["Yes", "No"])
 
-# Prediksi
 if st.button("prediksi", type="primary"):
     data_baru = pd.DataFrame([[hours_coding_daily, preferred_language, typing_speed, import_usage, oop_usage]],
                              columns=["hours_coding_daily", "preferred_language", "typing_speed", "import_usage", "oop_usage"])
@@ -32,4 +30,5 @@ if st.button("prediksi", type="primary"):
 
 st.divider()
 st.caption("Model Ini Di Buat Oleh **Fika💜💟**")
+
 
